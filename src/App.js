a2ea58015ea5a1ae1d70ef050bf32b14d9a70f1c
Igor Britto <igor.galvao@gmail.com>
2020-04-01 11:13:03 -0300

    Makes the app work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
+window.axios = axios;
+const responseDom = resp => <div className="responseBody">{resp}</div>;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { requests: [] };
+
+    this.addResponse = this.addResponse.bind(this);
+    this.makeRequest = this.makeRequest.bind(this);
+  }
+
+  addResponse(resp) {
+    this.setState({
+      requests: [...this.state.requests, <code>{JSON.stringify(resp)}</code>]
+    });
+  }
+
+  makeRequest(url) {
+    const { addResponse } = this;
+    axios
+      .get(url)
+      .then(function(response) {
+        console.log(response);
+        addResponse(response);
+      })
+      .catch(function(error) {
+        addResponse(error);
+        console.log(error);
+      });
+  }
+  render() {
+    const { makeRequest } = this;
+    const { requests } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              makeRequest(e.target.children.targetUrl.value);
+            }}
+          >
+            <input name="targetUrl"></input>
+            <input type="submit" />
+          </form>
+          <div className="responses">{requests.map(r => responseDom(r))}</div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
